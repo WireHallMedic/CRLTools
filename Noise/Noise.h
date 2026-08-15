eb9598e833b6e0aa8df04eef9d842c682a151640
Michael <MichaelWidler@yahoo.com>
2026-08-15 08:38:50 -0700

    Sanatize inputs.

--- Noise/Noise.h
+++ Noise/Noise.h
@@ -1,4 +1,9 @@
 #include <math.h>
 
 #define PI 3.14159265358979323846
-#define FIELD_SIZE = 23 // just need a reasonably-sized prime number to limit repetition
+#define FIELD_SIZE 23 // just need a reasonably-sized prime number to limit repetition
+
+double interpolateLinear(double p1, double p2, double xOff);
+double interpolateCosine(double p1, double p2, double xOff);
+double sanatizeIndex(double val);
+double reduceInput(double val);
--- Noise/NoiseField.c
+++ Noise/NoiseField.c
@@ -3,6 +3,10 @@
 #include "Noise.h"
 #define PI 3.14159265358979323846
 
+double primaryArray[FIELD_SIZE][FIELD_SIZE];
+double secondaryArray[FIELD_SIZE][FIELD_SIZE];
+double tertiaryArray[FIELD_SIZE][FIELD_SIZE];
+
 // returns the value of a point between two values.  For example, if the passed values are 2 and 4, and the xOffset
 // is .5 (halfway between the two), this will return 3.
 double interpolateLinear(double p1, double p2, double xOff)
@@ -17,4 +21,35 @@ double interpolateCosine(double p1, double p2, double xOff)
 {
    xOff = ((-1.0 * (float)cos(PI * xOff)) *.5) + .5;
    return interpolateLinear(p1, p2, xOff);
+}
+
+double sanatizeIndex(double val)
+{
+   // negative
+   if(val < 0)
+   {
+      val = -1.0 * val;
+      if(val > (double)FIELD_SIZE)
+      {
+         val = reduceInput(val);
+      }
+      val = (double)FIELD_SIZE - val;
+   }
+   // positive
+   else
+   if(val > (double)FIELD_SIZE)
+      val = reduceInput(val);
+   return val;
+}
+
+// input must be positive
+double reduceInput(double val)
+{
+   if(val > (double)FIELD_SIZE)
+   {
+      int val_int = (int)val % FIELD_SIZE;
+      double val_double = val - (double)((int)val);
+      val = val_int + val_double;
+   }
+   return val;
 }
